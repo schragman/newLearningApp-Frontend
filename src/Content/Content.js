2fc6992834f2bbc324ff4796d0b368935bbc5c87
schragman <schragman@gmail.com>
2024-05-02 07:45:16 +0200

    First draft with bootstrap

---
+++ src/Content/Content.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Content() {
+  return (
+    <div className="col-md-9 bg-dark" style={{ color: 'white', minHeight: '80vh' }}>
+      <h1>Hauptinhalt</h1>
+      <p>Willkommen auf Ihrer neuen Webseite!</p>
+    </div>
+  );
+}
+
+export default Content;
